7853eb1a680e3ae822f80a83d34ab0b69354baa9
Olavio Lacerda <olavio.junior@bolttech.io>
2021-09-14 22:01:28 -0300

    class(mocks): finished first class

---
+++ module2-js-testing/aula1-mocks/src/file.js
@@ -0,0 +1,65 @@
+const { readFile } = require('fs/promises')
+const { error } = require('./constants')
+const User = require('./user')
+
+const DEFAULT_OPTION = {
+  maxLines: 3,
+  fields: ["id","name","profession","age"]
+}
+
+class File {
+  static async csvToJson(filePath) {
+    const content = await File.getFileContent(filePath)
+    const validation = File.isValid(content)
+    if(!validation.valid) throw new Error(validation.error)
+    
+    const users = File.parseCSVToJson(content)
+    return users
+  }
+
+  static async getFileContent(filePath) {
+    return (await readFile(filePath)).toString("utf8")
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    const [header, ...fileWithoutHeader] = csvString.split('\n')
+    const isHeaderValid = header === options.fields.join(',')
+    if(!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+
+    const isContentLengthAccepted = (
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines
+    )
+    if(!isContentLengthAccepted) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+
+    return { valid: true }
+  }
+
+  static parseCSVToJson(csvString) {
+    const lines = csvString.split('\n')
+    // remove o primeiro item e joga na variavel
+    const firstLine = lines.shift()
+    const header = firstLine.split(',')
+    const users = lines.map(line => {
+      const columns = line.split(',')
+      let userToBeCreated = {}
+      for(const column in columns) {
+        userToBeCreated[header[column]] = columns[column];
+      }
+      return new User(userToBeCreated)
+    })
+    return users
+  }
+}
+
+module.exports = File
