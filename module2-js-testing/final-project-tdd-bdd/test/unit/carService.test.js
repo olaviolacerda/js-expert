84cc52fba7f5f86f67b3e5f7bd1b2bcf6dc2fa1b
Olavio Lacerda <olavio.junior@bolttech.io>
2022-05-13 09:17:25 -0300

    :tada: finish module2

--- module2-js-testing/final-project-tdd-bdd/test/unit/carService.test.js
+++ module2-js-testing/final-project-tdd-bdd/test/unit/carService.test.js
@@ -3,6 +3,7 @@ const { expect } = require('chai')
 const sinon = require('sinon')
 
 const CarService = require('./../../src/services/carService')
+const Transaction = require('./../../src/entities/transaction')
 const { mocks } = require('../mocks')
 
 const { join } = require('path')
@@ -39,7 +40,7 @@ describe('CarService suite tests', () => {
 		sandbox.stub(
 			carService,
 			carService.getRandomPositionFromArray.name
-		).returns(carIdIndex)
+			).returns(carIdIndex)
 
 		const result = carService.chooseRandomCar(carCategory)
 		const expected = carCategory.carIds[carIdIndex]
@@ -56,12 +57,12 @@ describe('CarService suite tests', () => {
 		sandbox.stub(
 			carService.carRepository,
 			carService.carRepository.find.name
-		).returns(car)
+			).returns(car)
 
 		sandbox.spy(
 			carService,
 			carService.chooseRandomCar.name
-		)
+			)
 
 		const result = await carService.getAvailableCar(carCategory)
 		const expected = car
@@ -69,5 +70,65 @@ describe('CarService suite tests', () => {
 		expect(carService.chooseRandomCar.calledOnce).to.be.ok
 		expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok	
 		expect(result).to.be.deep.equal(expected)
+	})
+
+	it('should calculate final amount in BRL when a carCategory, customer and numberOfDays are considered', async () => {
+		const customer = Object.create(mocks.validCustomer)
+		customer.age = 50
+
+		const carCategory = Object.create(mocks.validCarCategory)
+		carCategory.price = 37.6
+
+		const numberOfDays = 5
+
+		sandbox.stub(
+			carService,
+			"taxesBasedOnAge"
+			).get(()=> [{ from: 40, to: 50, then: 1.3}])
+
+		const expected = carService.currencyFormat.format(244.40)
+		const result = carService.calculateFinalPrice(
+			customer,
+			carCategory,
+			numberOfDays)
+
+		expect(result).to.be.deep.equal(expected)
+	})
+
+	it('should return a transaction receipt by a given customer and car category', async () => {
+		const car = mocks.validCar
+		const carCategory = {
+			...mocks.validCarCategory,
+			price: 37.6,
+			carIds: [car.id]
+		}
+
+		const customer = Object.create(mocks.validCustomer)
+		customer.age = 20
+
+		const numberOfDays = 5
+		const dueDate = "10 de novembro de 2020"
+
+		const now = new Date(2020, 10, 5)
+		sandbox.useFakeTimers(now.getTime())
+
+		sandbox.stub(
+			carService.carRepository,
+			carService.carRepository.find.name
+		).resolves(car)
+
+		const expectedAmount = carService.currencyFormat.format(206.80)
+		const result = await carService.rent(
+			customer, carCategory, numberOfDays
+		)
+		const expected = new Transaction({
+			customer,
+			car,
+			dueDate,
+			amount: expectedAmount,
+		})
+
+		expect(result).to.be.deep.equal(expected)
+
 	})
 })
