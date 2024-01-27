c6f240b9abf590215bdaa6d68513e010b5761c10
Olavio <olaviolacerdaa@gmail.com>
2024-01-27 14:01:49 -0300

    :art: js-testing mocks

--- module2-js-testing/aula1-mocks/src/constants.js
+++ module2-js-testing/aula1-mocks/src/constants.js
@@ -1,8 +1,8 @@
 const constants = {
   error: {
-    FILE_LENGTH_ERROR_MESSAGE: 'The content length is invalid!',
-    FILE_FIELDS_ERROR_MESSAGE: 'The provided properties are invalid!'
-  }
-}
+    FILE_LENGTH_ERROR_MESSAGE: "The content length is invalid!",
+    FILE_FIELDS_ERROR_MESSAGE: "The provided properties are invalid!",
+  },
+};
 
-module.exports = constants
+module.exports = constants;
--- module2-js-testing/aula1-mocks/src/user.js
+++ module2-js-testing/aula1-mocks/src/user.js
@@ -1,10 +1,10 @@
 class User {
   constructor({ name, id, profession, age }) {
-    this.name = name
-    this.id = parseInt(id)
-    this.profession = profession
-    this.birthDay = new Date().getFullYear() - age
+    this.name = name;
+    this.id = parseInt(id);
+    this.profession = profession;
+    this.birthDay = new Date().getFullYear() - age;
   }
 }
 
-module.exports = User
+module.exports = User;
