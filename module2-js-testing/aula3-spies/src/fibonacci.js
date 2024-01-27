a86c80e28b6e2d59a9d626e4a4c4e45b3a691804
Olavio <olaviolacerdaa@gmail.com>
2024-01-27 14:42:08 -0300

    :art: js-testing spies

--- module2-js-testing/aula3-spies/src/fibonacci.js
+++ module2-js-testing/aula3-spies/src/fibonacci.js
@@ -1,15 +1,10 @@
 class Fibonacci {
-  // generator
   *execute(input, current = 0, next = 1) {
-    if (input === 0) {
-      return 0
-    }
-    // retorna o valor
-    yield current
-    // delega a funcao, mas nao retorna valor
-    yield* this.execute(input - 1, next, current + next)
-
+    if (input === 0) return;
+    yield current;
+    // '*' delega a fn, mas nao retorna valor
+    yield* this.execute(input - 1, next, current + next);
   }
 }
 
-module.exports = Fibonacci
+module.exports = Fibonacci;
--- module2-js-testing/aula3-spies/src/fibonacci.test.js
+++ module2-js-testing/aula3-spies/src/fibonacci.test.js
@@ -1,48 +1,56 @@
-const Fibonacci = require('./fibonacci.js')
-const sinon = require('sinon')
-const assert = require('assert')
+// Fibonacci: o proximo nro da sequencia é sempre a soma dos anteriories
+// input: 3
+// 0,1,1
+// input 5
+// 0,1,1,2,3
+const { createSandbox } = require("sinon");
+const Fibonacci = require("./fibonacci");
+const sinon = createSandbox();
+const assert = require("assert");
 
-  // Fibonacci: o prox valor corresponde a soma dos dois anteriores
-  // dado 3
-  // 0,1,1
-  // dado 5
-  // 0,1,1,2,3
-
-  ;
 (async () => {
+  // Nro de sequencias : 5
+  // [0] input = 5, current = 0, next = 1, resultado = 0
+  // ....
+  // [5] input = 0 -> PARA
   {
-    const fibonacci = new Fibonacci()
-    const spy = sinon.spy(fibonacci, fibonacci.execute.name)
-    // generators retornam iterators, (.next)
-    // existem 3 formas de ler os dados
-    // usando as funcs .next, for await e rest/spread
-    for await (const i of fibonacci.execute(3)) {
-
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+    for (const sequencia of fibonacci.execute(5)) {
     }
-    // nosso algo comeca do zero
-    const expectedCallCount = 4
-    assert.deepStrictEqual(spy.callCount, expectedCallCount)
+    const expectedCallCount = 6;
+    assert.strictEqual(spy.callCount, expectedCallCount);
+    const { args } = spy.getCall(2);
+    const expectedParams = [3, 1, 2];
+    assert.deepStrictEqual(args, expectedParams, "os arrays não são iguais");
   }
+
   {
-    const fibonacci = new Fibonacci()
-    const spy = sinon.spy(fibonacci, fibonacci.execute.name)
-    const [...results] = fibonacci.execute(5)
-    // [0] input = 5, current = 0, next = 1
-    // [1] input = 4, current = 1, next = 1
-    // [2] input = 3, current = 1, next = 2
-    // [3] input = 2, current = 2, next = 3
-    // [4] input = 1, current = 3, next = 4
-    // [5] input = 0 -> PARA
-
-    const { args } = spy.getCall(2)
-    const expectedResult = [0, 1, 1, 2, 3]
-    const expectedParams = Object.values({
-      input: 3,
-      current: 1,
-      next: 2
-    })
-
-    assert.deepStrictEqual(args, expectedParams)
-    assert.deepStrictEqual(results, expectedResult)
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+    for (const sequencia of fibonacci.execute(3)) {
+    }
+    const expectedCallCount = 4;
+    assert.strictEqual(spy.callCount, expectedCallCount);
   }
-})()
+
+  {
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+
+    const results = [...fibonacci.execute(5)];
+
+    const expectedCallCount = 6;
+    assert.strictEqual(spy.callCount, expectedCallCount);
+    const { args } = spy.getCall(2);
+    const expectedParams = [3, 1, 2];
+    assert.deepStrictEqual(args, expectedParams, "os arrays não são iguais");
+
+    const expectedResults = [0, 1, 1, 2, 3];
+    assert.deepStrictEqual(
+      results,
+      expectedResults,
+      "os resultados sejam iguais"
+    );
+  }
+})();
